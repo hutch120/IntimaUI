00d333b842e2e5b55b537cd099cc5c946330c4ba
Simon <hutch120@gmail.com>
2018-05-18 15:39:13 +1000

    Render contacts

--- app/src/App.js
+++ app/src/App.js
@@ -1,61 +1,90 @@
 /* eslint */
 /* global fetch */
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import List, {ListItem, ListItemText} from 'material-ui/List'
 import logo from './logo.svg'
 import './App.css'
 import _ from 'lodash'
 
-const intimaDataURL = 'https://api.intima.io/data.json'
+const REACT_VERSION = React.version
+
+// const intimaDataURL = 'https://api.intima.io/data.json'
+const intimaDataURL =
+  'http://localhost:8080/api/v1/contacts/hutch120@gmail.com/token/7/1'
+
+// https://material-ui-next.com/demos/lists/
+function ContactList(props) {
+  const contacts = props.contacts
+  if (!_.isArray(contacts)) {
+    return (
+      <ListItem button component="a" href="#simple-list">
+        <ListItemText primary="{contacts}" />
+      </ListItem>
+    )
+  }
+  const contactItems = contacts.map(contact => (
+    <ListItem key={contact.toString()} button component="a" href="#simple-list">
+      <ListItemText primary={contact} />
+    </ListItem>
+  ))
+  return <List component="nav">{contactItems}</List>
+}
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
-    this.state = { data: 'Waiting for data from Intima.', count: 0 }
+    this.state = {contacts: 'Waiting for contacts from Intima.', count: 0}
   }
 
-  tick () {
+  tick() {
     var self = this
     fetch(intimaDataURL)
-      .then(function (response) {
+      .then(function(response) {
         self.setState(prevState => ({
           count: prevState.count + 1
         }))
 
         return response.json() // converts response to json
-      }).then(function (json) {
-        if (_.has(json, 'data')) {
+      })
+      .then(function(json) {
+        if (_.has(json, 'data') && _.get(json, 'success', false)) {
           self.setState(prevState => ({
-            data: json.data
+            contacts: json.data
           }))
         } else {
-          console.log('Invalid data.', json)
+          console.log('Invalid contacts.', json)
         }
-      }).catch(function (ex) {
+      })
+      .catch(function(ex) {
         console.log('parsing failed', ex, 'url', intimaDataURL)
       })
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.tick()
     this.interval = setInterval(() => this.tick(), 5000)
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     clearInterval(this.interval)
   }
 
-  render () {
+  render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to Intima UI</h1>
-          <h1 className='App-title-h4'>Making online better for you.</h1>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Intima UI</h1>
+          <h1 className="App-title-h4">Making online better for you.</h1>
+          <div>React version: {REACT_VERSION}</div>
         </header>
-        <p><b>The data below is being polled from Intima every 5 seconds</b></p>
+        <p>
+          <b>The data below is being polled from Intima every 5 seconds</b>
+        </p>
         <p>URL {intimaDataURL}</p>
-        <p>{this.state.data}</p>
+        <ContactList contacts={this.state.contacts} />
+
         <p>Request count: {this.state.count}</p>
       </div>
     )
